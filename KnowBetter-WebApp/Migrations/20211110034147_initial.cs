e29a35282285fb9914ba4eb5d2638874e4246055
moe-mohammed <73504097+moe-mohammed@users.noreply.github.com>
2021-11-10 17:41:26 -0800

    Initializing repo

---
+++ KnowBetter-WebApp/Migrations/20211110034147_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KnowBetter_WebApp.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ KnowBetter-WebApp/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowBetter_WebApp.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Since there's no array type mapping in EF, we'll use a string with ingredients separated using commas.
+        // This string will be stored in the database called InternalAvoidIngredients.
+        // AvoidIngredients is the C# Array that we will use to access that string to retrieve list of avoid ingredients and modify it.
+        private string InternalAvoidIngredients { get; set; }
+
+        [NotMapped]
+        public string[] AvoidIngredients
+        {
+            get { return InternalAvoidIngredients.Split(','); }
+            set
+            {
+                AvoidIngredients = value;
+                InternalAvoidIngredients = String.Join(",", AvoidIngredients.Select(p => p.ToString()).ToArray());
+            }
+        }
+    }
+}
