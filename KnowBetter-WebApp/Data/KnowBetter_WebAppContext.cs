a626d2f592f434ed21c27e95bf91e0896c61afa7
moe-mohammed <73504097+moe-mohammed@users.noreply.github.com>
2021-11-10 17:41:26 -0800

    Initializing repo

---
+++ KnowBetter-WebApp/Data/KnowBetter_WebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KnowBetter_WebApp.Models;
+
+namespace KnowBetter_WebApp.Data
+{
+    public class KnowBetter_WebAppContext : DbContext
+    {
+        public KnowBetter_WebAppContext (DbContextOptions<KnowBetter_WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KnowBetter_WebApp.Models.User> User { get; set; }
+    }
+}
