3955425c25ced8d7b6017fb369e1dc87149c3410
moe-mohammed <73504097+moe-mohammed@users.noreply.github.com>
2021-11-21 13:21:29 -0800

    Added all necessary models for entity framework and updated entity framework context.

---
+++ KnowBetter-WebApp/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowBetter_WebApp.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string IngredientName { get; set; }
+    }
+}
