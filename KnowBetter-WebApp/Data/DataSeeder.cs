8e24bfa62ed06baecce32a9b8f50773832603003
jraobc <j.rao@bellevuecollege.edu>
2021-11-24 15:48:26 -0800

    Adds ParseSeed() method to SeedReader. Reads .csv file and inserts into database. Also add test user.

--- KnowBetter-WebApp/Data/DataSeeder.cs
+++ KnowBetter-WebApp/Data/DataSeeder.cs
@@ -1,16 +1,95 @@
 ﻿using KnowBetter_WebApp.Models;
 using Microsoft.EntityFrameworkCore;
-
+using System.Linq;
+using System.IO;
 
 namespace KnowBetter_WebApp.Data
 {
     public class DataSeeder
     {
-        public static void SeedProducts(DbContext context)
+        public static void SeedProducts(KnowBetter_WebAppContext context)
         {
-            var newProduct = new Product {ProductName = "Test Product 1"};
-            context.Add(newProduct);
-            context.SaveChanges();
+            if (!context.Product.Any())
+            {
+                
+                /*  Seed file must be prepared before use:
+                 *  1. Remove headings
+                 *  2. Find and replace single quote (') with 2 single quotes ('')
+                 */
+                ParseSeed(".\\Data\\KBSeed1.csv", context);
+
+                //Add test user for development purposes
+                User testUser = new User
+                {
+                    UserId = "TestUserId",
+                    FirstName = "TestFirstName",
+                    LastName = "TestLastName",
+                    Email = "test@test.com",
+                    Age = "22",
+                    Password = "TestPassword"
+                };
+                context.User.Add(testUser);
+                context.SaveChanges();
+            }
+            
+        }
+
+        public static void ParseSeed(string filePath, KnowBetter_WebAppContext context)
+        {
+            var lines = File.ReadLines(filePath);
+            string[] lineArray;
+            string productName;
+            string brand;
+            int productId;
+            Ingredient ingredient;
+            int ingredientId;
+            string ingredientName;
+            Product product;
+            ProductIngredient productIngredient;
+
+            foreach (string line in lines)
+            {
+                //read line
+                lineArray = line.Split(",");
+                brand = lineArray[0];
+                productName = lineArray[1];
+
+                //add product and get product ID
+                product = new Product { Brand = brand, ProductName = productName };
+                context.Add(product);
+                context.SaveChanges();
+                productId = product.ProductId;
+
+                //add ingredients and get ingredient IDs
+                for (int i = 2; i < lineArray.Length; i++)
+                {
+                    ingredientName = lineArray[i];
+
+
+                    //check if ingredient already exists
+                    try
+                    {
+                        ingredient = context.Ingredient.Single(b => b.IngredientName == ingredientName);
+                    }
+                    //add ingredient if it doesn't exist
+                    catch (System.InvalidOperationException)
+                    {
+                        ingredient = new Ingredient { IngredientName = ingredientName };
+                        context.Add(ingredient);
+                        context.SaveChanges();
+                    }
+                    //get IngredientId
+                    ingredientId = ingredient.IngredientId;
+
+                    //Add ProductIngredient entry
+                    productIngredient = new ProductIngredient { ProductId = productId, IngredientId = ingredientId };
+                    context.ProductIngredient.Add(productIngredient);
+                }
+
+                context.SaveChanges();
+            }
+
+            
         }
     }
 }
