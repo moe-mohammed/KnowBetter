a244c2c7ea869ed31697d03417886aac0a73213d
jraobc <j.rao@bellevuecollege.edu>
2021-12-09 21:06:54 -0800

    Removes testing code

--- KnowBetter-WebApp/Controllers/HomeController.cs
+++ KnowBetter-WebApp/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,7 +25,6 @@ namespace KnowBetter_WebApp.Controllers
 
         public IActionResult Dashboard()
         {
-            Debug.WriteLine("From session: " + HttpContext.Session.GetString(key: "_Name"));
             ViewBag.MyName = HttpContext.Session.GetString(key: "_Name");
 
             return View();
