508d14a7412272c01126b894330272d5500972a2
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2021-12-12 11:57:44 -0800

    Added logos. Added contact form

---
+++ KnowBetter-WebApp/Controllers/ContactFormController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnowBetter_WebApp.Models;
+
+namespace KnowBetter_WebApp.Controllers
+{
+    public class ContactFormController : Controller
+    {
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Thanks()
+        {
+            return View("Thanks");
+        }
+
+        private EmailAddress FromAndToEmailAddress;
+        private IEmailService EmailService;
+        public ContactFormController(EmailAddress _fromAddress,
+            IEmailService _emailService)
+        {
+            FromAndToEmailAddress = _fromAddress;
+            EmailService = _emailService;
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                EmailMessage msgToSend = new EmailMessage
+                {
+                    FromAddresses = new List<EmailAddress> { FromAndToEmailAddress },
+                    ToAddresses = new List<EmailAddress> { FromAndToEmailAddress },
+                    Content = $"Here is your message: Name: {model.Name}, " +
+                        $"Email: {model.Email}, \n Message: {model.Message}",
+                    Subject = "Contact Form - KnowBetter App"
+                };
+
+                EmailService.Send(msgToSend);
+                return RedirectToAction("Thanks");
+            }
+            else
+            {
+                return Index();
+            }
+        }
+    }
+}
